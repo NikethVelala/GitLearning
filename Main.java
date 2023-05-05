60a347407076200c24b50a0ba62dd66e714fe0e6
Niketh Velala <velalaniketh@gmail.com>
2023-05-05 10:21:08 +0530

    Made new changes for newbranch

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Here we have cut a branch from \"main\" named \"newbranch\"");
         System.out.println("Here we have cut a branch from \"main\" named \"anotherNewBranch\"");
+
+        System.out.println("Made changes for newbranch");
     }
 }
